277772f8b2229dc2538113444fb12ba3f487df6e
Guilherme Lima Santos <grguilh2rm2lima@gmail.com>
2025-08-12 15:17:56 -0300

    prod: publicação do app

--- Codigo/app.py
+++ Codigo/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from routes.home import home_bp
+import os
 
 app = Flask(__name__)
 
 app.register_blueprint(home_bp)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.getenv('PORT'), '5000')
+    app.run(host='0.0.0.0', port=port)
